855685bf0be5c51e21ae6a82f7eddba6fba950e7
Chuck Remes <cremes@mac.com>
2010-05-16 23:11:00 -0500

    remove debug line

--- lib/ffi-rzmq/poll.rb
+++ lib/ffi-rzmq/poll.rb
@@ -30,7 +30,6 @@ module ZMQ
     def poll timeout = :blocking
       unless @items.empty?
         timeout = adjust timeout
-        puts "items [#{@items.size}]"
         items_triggered = LibZMQ.zmq_poll @items.address, @items.size, timeout
         error_check ZMQ_POLL_STR, items_triggered >= 0 ? 0 : items_triggered
         update_selectables
