80d8de2e6923854e44f69705f2202e5367b32b71
Chuck Remes <cremes@mac.com>
2010-05-26 13:45:03 -0500

    added specs and rdoc for Context

--- lib/ffi-rzmq/context.rb
+++ lib/ffi-rzmq/context.rb
@@ -5,11 +5,27 @@ module ZMQ
   ZMQ_TERM_STR = 'zmq_term'.freeze
   ZMQ_SOCKET_STR = 'zmq_socket'.freeze unless defined? ZMQ_SOCKET_STR
 
+
   class Context
     include ZMQ::Util
 
     attr_reader :context, :pointer
 
+    # Recommended to just pass 1 for +app_threads+ and +io_threads+
+    # since most programs are not heavily threaded. The rule of thumb
+    # is to make +app_threads+ equal to the number of application
+    # threads that will be accessing 0mq sockets within this context.
+    #
+    # Takes a single flag (soon to be deprecated): #ZMQ::POLL
+    #
+    # Returns a context object. It's necessary for passing to the
+    # #Socket constructor when allocating new sockets. All sockets
+    # live within a context. Sockets in one context may not be accessed
+    # from another context's app_threads; doing so raises an exception.
+    #
+    # To connect sockets between contexts, use +inproc+ or +ipc+
+    # transport and set up a 0mq socket between them.
+    #
     # May raise a #ContextError.
     #
     def initialize app_threads, io_threads, flags = 0
@@ -17,8 +33,17 @@ module ZMQ
       @context = LibZMQ.zmq_init app_threads, io_threads, flags
       @pointer = @context
       error_check ZMQ_INIT_STR, @context.null? ? 1 : 0
+
+      define_finalizer
     end
 
+    # Call to release the context and any remaining data associated
+    # with past sockets. This will close any sockets that remain
+    # open; further calls to those sockets will raise failure
+    # exceptions.
+    #
+    # Returns nil.
+    #
     # May raise a #ContextError.
     #
     def terminate
@@ -29,12 +54,36 @@ module ZMQ
       @sockets = nil
     end
 
+    # Short-cut to allocate a socket for a specific context.
+    #
+    # Takes several +type+ values:
+    #   #ZMQ::REQ
+    #   #ZMQ::REP
+    #   #ZMQ::PUB
+    #   #ZMQ::SUB
+    #   #ZMQ::PAIR
+    #   #ZMQ::UPSTREAM
+    #   #ZMQ::DOWNSTREAM
+    #
+    # Returns a #ZMQ::Socket.
+    #
     # May raise a #ContextError or #SocketError.
     #
     def socket type
       @socket = Socket.new @context, type
       error_check ZMQ_SOCKET_STR, @socket.nil? ? 1 : 0
       @socket
+    end
+
+
+    private
+
+    def define_finalizer
+      ObjectSpace.define_finalizer(self, self.class.close(@context))
+    end
+
+    def self.close context
+      Proc.new { LibZMQ.zmq_term context unless context.nil? }
     end
   end
 
--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -4,24 +4,77 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 module ZMQ
 
 
-    describe Context do
+  describe Context do
 
-      context "when initializing" do
-        include APIHelper
-        
-        before(:all) do
-          #stub_libzmq
-        end
-        
-        it "should raise an error for negative app threads" do
-          lambda { Context.new -1, -1, 0 }.should raise_exception(ZMQ::ContextError)
-        end
-        
-        it "should not raise an error for positive thread counts" do
-          lambda { Context.new 1, 1, 0 }.should_not raise_error
-        end
-      end # context initializing
-    end
+    context "when initializing" do
+      include APIHelper
 
-    
+      before(:all) do
+        #stub_libzmq
+      end
+
+      it "should raise an error for negative app threads" do
+        lambda { Context.new -1, -1, 0 }.should raise_exception(ZMQ::ContextError)
+      end
+
+      it "should raise an error for negative io threads" do
+        lambda { Context.new 1, -1, 0 }.should raise_exception(ZMQ::ContextError)
+      end
+
+      it "should not raise an error for positive thread counts" do
+        lambda { Context.new 1, 1, 0 }.should_not raise_error
+      end
+
+      it "should set the :pointer accessor to non-nil" do
+        ctx = Context.new 1, 1
+        ctx.pointer.should_not be_nil
+      end
+
+      it "should set the :context accessor to non-nil" do
+        ctx = Context.new 1, 1
+        ctx.context.should_not be_nil
+      end
+
+      it "should set the :pointer and :context accessors to the same value" do
+        ctx = Context.new 1, 1
+        ctx.pointer.should == ctx.context
+      end
+      
+      it "should define a finalizer on this object" do
+        ObjectSpace.should_receive(:define_finalizer)
+        ctx = Context.new 1, 1
+      end
+    end # context initializing
+
+
+    context "when terminating" do
+      it "should call zmq_term to terminate the library's context" do
+        ctx = Context.new 1, 1
+        LibZMQ.should_receive(:zmq_term).with(ctx.pointer).and_return(0)
+        ctx.terminate
+      end
+
+      it "should raise an exception when it fails" do
+        ctx = Context.new 1, 1
+        LibZMQ.stub(:zmq_term => 1)
+        lambda { ctx.terminate }.should raise_error(ZMQ::ContextError)
+      end
+    end # context terminate
+
+
+    context "when allocating a socket" do
+      it "should return a ZMQ::Socket" do
+        ctx = Context.new 1, 1
+        ctx.socket(ZMQ::REQ).should be_kind_of(ZMQ::Socket)
+      end
+
+      it "should raise an exception when allocation fails" do
+        ctx = Context.new 1, 1
+        Socket.stub(:new => nil)
+        lambda { ctx.socket(ZMQ::REQ) }.should raise_error(ZMQ::SocketError)
+      end
+    end # context socket
+  end
+
+
 end # module ZMQ
