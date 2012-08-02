ad90ef564658540d0804db544ed0927231a0c7cf
Chuck Remes <git@chuckremes.com>
2012-08-02 14:19:46 -0500

    zmq_device was added back to libzmq for 3.1, so support it unconditionally

--- lib/ffi-rzmq/device.rb
+++ lib/ffi-rzmq/device.rb
@@ -1,28 +1,28 @@
 module ZMQ
-  if LibZMQ.version2?
-    class Device
-      attr_reader :device
-      
-      def self.create(device_type, frontend, backend)
+
+  class Device
+    attr_reader :device
+
+    def self.create(device_type, frontend, backend)
+      dev = nil
+      begin
+        dev = new(device_type, frontend, backend)
+      rescue ArgumentError
         dev = nil
-        begin
-          dev = new device_type, frontend, backend
-        rescue ArgumentError
-          dev = nil
-        end
-        
-        dev
       end
 
-      def initialize(device_type,frontend,backend)
-        [["frontend", frontend],["backend", backend]].each do |name,socket|
-          unless socket.is_a?(ZMQ::Socket)
-            raise ArgumentError, "Expected a ZMQ::Socket, not a #{socket.class} as the #{name}"
-          end
-        end
+      dev
+    end
 
-        LibZMQ.zmq_device(device_type, frontend.socket, backend.socket)
+    def initialize(device_type, frontend, backend)
+      [["frontend", frontend], ["backend", backend]].each do |name, socket|
+        unless socket.is_a?(ZMQ::Socket)
+          raise ArgumentError, "Expected a ZMQ::Socket, not a #{socket.class} as the #{name}"
+        end
       end
+
+      LibZMQ.zmq_device(device_type, frontend.socket, backend.socket)
     end
   end
+
 end
