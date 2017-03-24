b22b7a9ff12fdafee1baf79b01f5beec465e3a82
Chuck Remes <git@chuckremes.com>
2017-03-24 15:00:12 -0500

    go straight to source for libsodium

--- travis_build_script.sh
+++ travis_build_script.sh
@@ -4,7 +4,13 @@ set -ev
 bundle install
 
 sudo apt-get -qq update
-sudo apt-get install libtool pkg-config build-essential autoconf automake wget libsodium18 libsodium-dev
+sudo apt-get install libtool pkg-config build-essential autoconf automake wget 
+
+git clone https://github.com/jedisct1/libsodium --branch stable
+cd libsodium
+./autogen.sh
+./configure && make check
+sudo make install
 
 wget http://download.zeromq.org/zeromq-4.2.2.tar.gz
 tar -xvf zeromq-4.2.2.tar.gz
